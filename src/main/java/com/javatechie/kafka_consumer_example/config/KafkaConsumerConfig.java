5b0fea90314f1f7764eabfebd410e3fe32933cd3
rohitginnare <rohitmalakar07@gmail.com>
2024-06-20 19:12:52 +0530

    Implemented Consumer to consumer from single partition

---
+++ src/main/java/com/javatechie/kafka_consumer_example/config/KafkaConsumerConfig.java
@@ -0,0 +1,42 @@
+package com.javatechie.kafka_consumer_example.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+	@Bean
+	public Map<String, Object> consumerConfig() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "retry-group");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		props.put(org.springframework.kafka.support.serializer.JsonDeserializer.TRUSTED_PACKAGES, "*");
+		return props;
+	}
+
+	@Bean
+	public ConsumerFactory<String, Object> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfig());
+	}
+
+	@Bean
+	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+}
--- src/main/java/com/javatechie/kafka_consumer_example/consumer/KafkaMessageListener.java
+++ src/main/java/com/javatechie/kafka_consumer_example/consumer/KafkaMessageListener.java
@@ -2,10 +2,14 @@ package com.javatechie.kafka_consumer_example.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-
-import com.javatechie.kafka_consumer_example.dto.Customer;
 
 @Service
 public class KafkaMessageListener {
@@ -32,9 +36,32 @@ public class KafkaMessageListener {
 //		log.info("Consumer4 consumed message {}", message);
 //	}
 
-	@KafkaListener(topics = "javatechie-customer", groupId = "jt-group-new-customer")
-	public void consume(Customer customer) {
-		log.info("Consumed data from Customer Object...");
+//	@KafkaListener(topics = "javatechie-customer-1", groupId = "jt-group-new-customer")
+//	public void consume(com.kafka_producer_example.dto.Customer customer) {
+//		log.info("Consumed data from Customer Object...: " + customer.toString());
+//	}
+
+	// consuming message on single partition of a topic
+	@RetryableTopic(attempts = "3") //
+	@KafkaListener(topics = "retry-topic", groupId = "retry-group", topicPartitions = {
+			@TopicPartition(topic = "single-partition-message-send", partitions = "3") })
+	public void consumeOnSinglePartition(String message) {
+		String temp = "test";
+		try {
+			if (temp.equalsIgnoreCase("test")) {
+				throw new RuntimeException("Message can not be consumed");
+			} else {
+				log.info("Message Consumed successfully..{}", message);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@DltHandler
+	public void listenDLT(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+			@Header(KafkaHeaders.OFFSET) String offset) {
+		log.info("DLT received : {} from {}, offset {}", message, topic, offset);
 	}
 
 }
